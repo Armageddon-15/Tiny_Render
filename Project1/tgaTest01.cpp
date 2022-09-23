475e3a654654c5454ce3dd6f538faebd45aca4e7
Armageddon-15 <hjc014@126.com>
2022-09-23 22:21:03 +0800

    improve performance of drawing line

--- Project1/tgaTest01.cpp
+++ Project1/tgaTest01.cpp
@@ -19,23 +19,29 @@ void BresenhamLine01(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor c
 
 }
 
-
-void BresenhamLine02(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color){
-	// first improvement
+bool isSteep(int& x0, int& y0, int& x1, int& y1){
 	bool steep = false;
 
 	if (abs(x0 - x1) < abs(y0 - y1)){
-		std::swap(x0, y0);
-		std::swap(x1, y1);
-		steep = true;
+			std::swap(x0, y0);
+			std::swap(x1, y1);
+			steep = true;
 	}
-	// select a longer path
+		// select a longer path
 
 	if (x0 > x1){
 		std::swap(x0, x1);
 		std::swap(y0, y1);
 	}
 	// keep the smaller one at the first
+
+	return steep;
+}
+
+
+void BresenhamLine02(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color){
+	// first improvement
+	bool steep = isSteep(x0, y0, x1, y1);
 
 	for (int x = x0; x < x1; x++){
 		float t = (x - x0) / (float)(x1 - x0);
@@ -53,28 +59,61 @@ void BresenhamLine02(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor c
 
 void BresenhamLine03(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color){
 	// second improvement
-	bool steep = false;
+	// speed up
+	bool steep = isSteep(x0, y0, x1, y1);
 
-	if (abs(x0 - x1) < abs(y0 - y1)){
-		std::swap(x0, y0);
-		std::swap(x1, y1);
-		steep = true;
+	int dx = x1 - x0;
+	int dy = y1 - y0;
+
+	float derror = abs(dy / (float)dx); // const
+	float error = 0;
+
+	int y = y0;
+	for (int x = x0; x < x1; x++){
+		if (steep)
+			image.set(y, x, color); 
+		else
+			image.set(x, y, color);
+
+		error += derror;
+		if(error > 0.5f){
+			y += y1 > y0 ? 1 : -1;
+			error -= 1.0f;
+		}
 	}
-	// select a longer path
+}
 
-	if (x0 > x1){
-		std::swap(x0, x1);
-		std::swap(y0, y1);
+void BresenhamLine04(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color){
+	// third improvement
+	// speed up
+	bool steep = isSteep(x0, y0, x1, y1);
+
+	int dx = x1 - x0;
+	int dy = y1 - y0;
+
+	int derror = abs(dy) * 2; // const
+	int error = 0;
+
+	int y = y0;
+	for (int x = x0; x < x1; x++){
+		if (steep)
+			image.set(y, x, color); 
+		else
+			image.set(x, y, color);
+
+		error += derror;
+		if(error > dx){
+			y += y1 > y0 ? 1 : -1;
+			error -= dx * 2;
+		}
 	}
-	// keep the smaller one at the first
-
-
 }
 
 
 int main(){
 	TGAImage image(100, 100, TGAImage::RGB);
-	BresenhamLine02(1, 1, 20, 1, image, red);
+	BresenhamLine04(1, 1, 76, 94, image, red);
+	BresenhamLine03(2, 84, 76, 3, image, white);
 	image.write_tga_file("../bb2.tga");
 	return 0;
 }
