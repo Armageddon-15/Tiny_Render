80fb861a6e4935098ebc8535ec082678480e82a9
Armageddon-15 <hjc014@126.com>
2023-03-23 01:24:32 +0800

    Update tgaTest01.cpp

--- Project1/tgaTest01.cpp
+++ Project1/tgaTest01.cpp
@@ -16,7 +16,6 @@ void BresenhamLine01(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor c
 		int y = y0 + (y1 - y0) * i;
 		image.set(x, y, color);
 	}
-
 }
 
 bool isSteep(int& x0, int& y0, int& x1, int& y1){
@@ -38,6 +37,12 @@ bool isSteep(int& x0, int& y0, int& x1, int& y1){
 	return steep;
 }
 
+template<typename T>
+inline T lerp(T &a, T &b, float &alpha) {
+	// return a if alpha is 0
+	return a * (1 - alpha) + b * alpha;
+}
+
 
 void BresenhamLine02(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color){
 	// first improvement
@@ -45,7 +50,7 @@ void BresenhamLine02(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor c
 
 	for (int x = x0; x < x1; x++){
 		float t = (x - x0) / (float)(x1 - x0);
-		int y = y0 * (1.0f - t) + (y1 * t);
+		int y = lerp(y0, y1, t);
 
 		if (steep)
 			image.set(y, x, color);
@@ -95,16 +100,23 @@ void BresenhamLine04(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor c
 	int error = 0;
 
 	int y = y0;
-	for (int x = x0; x < x1; x++){
-		if (steep)
+	if (steep) {
+		for (int x = x0; x < x1; x++){
 			image.set(y, x, color); 
-		else
+			error += derror;
+			if(error > dx){
+				y += y1 > y0 ? 1 : -1;
+				error -= dx * 2;
+			}
+		}
+	} else {
+		for (int x = x0; x < x1; x++){
 			image.set(x, y, color);
-
-		error += derror;
-		if(error > dx){
-			y += y1 > y0 ? 1 : -1;
-			error -= dx * 2;
+			error += derror;
+			if(error > dx){
+				y += y1 > y0 ? 1 : -1;
+				error -= dx * 2;
+			}
 		}
 	}
 }
